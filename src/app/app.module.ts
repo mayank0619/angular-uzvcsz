c06208cdc02d031ec8fdf4a2031d505fdb0df210
Mayank Gupta <mayank.gupta@ranosys.com>
2019-05-28 18:55:55 +0530

    Worked on brand listing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BrandListingComponent } from './brand-listing/brand-listing.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BrandListingComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [BrandListingComponent]
 })
 export class AppModule { }
---
+++ src/app/brand-listing/brand-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-listing',
+  templateUrl: './brand-listing.component.html',
+  styleUrls: ['./brand-listing.component.css']
+})
+export class BrandListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
